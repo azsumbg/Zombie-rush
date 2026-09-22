c9a55d2b186cef997056ef9d2ce0114776e6587b
Daniel Dinev <azsumbg@gmail.com>
2026-09-22 07:36:59 +0300

    Improve resource management and error handling
    Add ExplosionFrame for animation control. Introduce HasRelease concept and FreeMem template for safe COM release. Implement LogErr for error logging. Add FreeResources to release all Direct2D/DirectWrite resources and log failures. Update ErrExit to use new utilities for cleanup and reporting. Refactor wWinMain for better init, error checking, and cleanup. Remove bmpRock1/bmpRock2 variables and delete rock1.png/rock2.png.

--- Zombie rush/Zombie rush.cpp
+++ Zombie rush/Zombie rush.cpp
@@ -111,8 +111,6 @@ IDWriteTextFormat* bigText{ nullptr };
 ID2D1Bitmap* bmpCastle{ nullptr };
 ID2D1Bitmap* bmpEnergyField1{ nullptr };
 ID2D1Bitmap* bmpEnergyField2{ nullptr };
-ID2D1Bitmap* bmpRock1{ nullptr };
-ID2D1Bitmap* bmpRock2{ nullptr };
 ID2D1Bitmap* bmpSand{ nullptr };
 
 ID2D1Bitmap* logoLevel{ nullptr };
@@ -139,10 +137,98 @@ ID2D1Bitmap* bmpShot[4]{ nullptr };
 
 /////////////////////////////////////////////////////////
 
+int ExplosionFrame()
+{
+	static int frame_delay = 3;
+	static int frame = 0;
 
+	--frame_delay;
+	if (frame_delay <= 0)
+	{
+		frame_delay = 3;
+		++frame;
+		if (frame > 23)return -1;
+	}
 
+	return frame;
+}
 
+template<typename T>concept HasRelease = requires(T var)
+{
+	var.Release();
+};
+template<HasRelease T>bool FreeMem(T** var)
+{
+	if ((*var))
+	{
+		(*var)->Release();
+		(*var) = nullptr;
+		return true;
+	}
 
+	return false;
+}
+void LogErr(const wchar_t* what)
+{
+	std::wofstream err(L".\\res\\data\\error.log", std::ios::app);
+	err << what << L" time stamp: " << std::chrono::system_clock::now() << std::endl;
+	err.close();
+}
+void FreeResources()
+{
+	if (!FreeMem(&iFactory))LogErr(L"Error releasing main D2D1Factory !");
+	if (!FreeMem(&Draw))LogErr(L"Error releasing main D2D1HwndRenderTarget !");
+
+	if (!FreeMem(&statBrush))LogErr(L"Error releasing main D2D1 statBrush !");
+	if (!FreeMem(&txtBrush))LogErr(L"Error releasing main D2D1 txtBrush !");
+	if (!FreeMem(&hgltBrush))LogErr(L"Error releasing main D2D1 hgltBrush !");
+	if (!FreeMem(&inactBrush))LogErr(L"Error releasing main D2D1 inactBrush !");
+
+	if (!FreeMem(&b1BckgBrush))LogErr(L"Error releasing main D2D1 b1BckgBrush !");
+	if (!FreeMem(&b2BckgBrush))LogErr(L"Error releasing main D2D1 b2BckgBrush !");
+	if (!FreeMem(&b3BckgBrush))LogErr(L"Error releasing main D2D1 b3BckgBrush !");
+
+	if (!FreeMem(&iWriteFactory))LogErr(L"Error releasing main D2D1WriteFactory !");
+	if (!FreeMem(&nrmText))LogErr(L"Error releasing main nrmText !");
+	if (!FreeMem(&midText))LogErr(L"Error releasing main midText !");
+	if (!FreeMem(&bigText))LogErr(L"Error releasing main bigText !");
+
+	if (!FreeMem(&bmpCastle))LogErr(L"Error releasing bmpCastle !");
+	if (!FreeMem(&bmpEnergyField1))LogErr(L"Error releasing bmpEnergyField1 !");
+	if (!FreeMem(&bmpEnergyField2))LogErr(L"Error releasing bmpEnergyField2 !");
+	if (!FreeMem(&bmpSand))LogErr(L"Error releasing bmpSand !");
+
+	if (!FreeMem(&logoLevel))LogErr(L"Error releasing logoLevel !");
+	if (!FreeMem(&logoLoose))LogErr(L"Error releasing logoLoose !");
+	if (!FreeMem(&logoWin))LogErr(L"Error releasing logoWin !");
+	if (!FreeMem(&logoRecord))LogErr(L"Error releasing logoRecord !");
+
+	for (int i = 0; i < 16; ++i)if (!FreeMem(&bmpZombie1[i]))LogErr(L"Error releasing bmpZombie1 !");
+	for (int i = 0; i < 14; ++i)if (!FreeMem(&bmpZombie2[i]))LogErr(L"Error releasing bmpZombie2 !");
+	for (int i = 0; i < 21; ++i)if (!FreeMem(&bmpZombie3[i]))LogErr(L"Error releasing bmpZombie3 !");
+
+	for (int i = 0; i < 24; ++i)if (!FreeMem(&bmpExplosion[i]))LogErr(L"Error releasing bmpExplosion !");
+
+	for (int i = 0; i < 8; ++i)if (!FreeMem(&bmpVitWarrior[i]))LogErr(L"Error releasing bmpVitWarrior !");
+	for (int i = 0; i < 8; ++i)if (!FreeMem(&bmpFallWarrior[i]))LogErr(L"Error releasing bmpFallWarrior !");
+
+	for (int i = 0; i < 8; ++i)if (!FreeMem(&bmpVitMage[i]))LogErr(L"Error releasing bmpVitMage !");
+	for (int i = 0; i < 8; ++i)if (!FreeMem(&bmpFallMage[i]))LogErr(L"Error releasing bmpFallMage !");
+
+	for (int i = 0; i < 17; ++i)if (!FreeMem(&bmpIntro[i]))LogErr(L"Error releasing bmpIntro !");
+	for (int i = 0; i < 20; ++i)if (!FreeMem(&bmpOcean[i]))LogErr(L"Error releasing bmpOcean !");
+	for (int i = 0; i < 4; ++i)if (!FreeMem(&bmpPause[i]))LogErr(L"Error releasing bmpPause !");
+	for (int i = 0; i < 4; ++i)if (!FreeMem(&bmpShot[i]))LogErr(L"Error releasing bmpShot !");
+}
+void ErrExit(int what)
+{
+	MessageBeep(MB_ICONERROR);
+	MessageBox(NULL, ErrHandle(what), L"Критична грешка !", MB_OK | MB_APPLMODAL | MB_ICONERROR);
+
+	FreeResources();
+	std::remove(tmp_file);
+	exit(1);
+}
 
 
 
@@ -152,12 +238,23 @@ ID2D1Bitmap* bmpShot[4]{ nullptr };
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
-    
+	bIns = hInstance;
+	if (!bIns)
+	{
+		LogErr(L"Error in Windows hInstance !");
+		ErrExit(eClass);
+	}
 
 
 
 
 
 
-    return (int) msg.wParam;
+
+
+
+	FreeResources();
+	std::remove(tmp_file);
+
+    return (int) bMsg.wParam;
 }
